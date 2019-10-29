3139d7edb7d240bbb063bb8b22c128826e675d11
Cyonopex <hfg318@gmail.com>
2019-10-30 02:10:21 +0800

    Merged both APIs into one file

--- WEBSITE/cleanup.bat
+++
@@ -1,5 +0,0 @@
-mkdir backup\%1
-mkdir backup\%1\temp
-
-MOVE /Y images\* backup\%1
-MOVE /Y temp\* backup\%1\temp
--- server/main.py
+++ server/main.py
@@ -1,16 +1,40 @@
 import os
+#import magic
 import urllib.request
 from app import app
-from flask import Flask, request, redirect, jsonify
+from flask import Flask, flash, request, redirect, render_template, jsonify
 from werkzeug.utils import secure_filename
+from threading import Thread
+import stitchHandler
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
+@app.route('/')
+def upload_form():
+	return render_template('upload.html')
 
-@app.route('/upload', methods=['POST'])
+@app.route('/', methods=['POST'])
 def upload_file():
+	if request.method == 'POST':
+        # check if the post request has the files part
+		if 'files[]' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		files = request.files.getlist('files[]')
+		for file in files:
+			if file and allowed_file(file.filename):
+				filename = secure_filename(file.filename)
+				file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		flash('File(s) successfully uploaded')
+		thread = Thread(target=stitchHandler.stitch)
+		thread.start()
+		return redirect('/')
+		
+@app.route('/upload', methods=['POST'])
+def upload_file_API():
 	# check if the post request has the file part
 	if 'files[]' not in request.files:
 		resp = jsonify({'message' : 'No file part in the request'})
@@ -38,6 +62,8 @@ def upload_file():
 	if success:
 		resp = jsonify({'message' : 'Files successfully uploaded'})
 		resp.status_code = 201
+		thread = Thread(target=stitchHandler.stitch)
+		thread.start()
 		return resp
 	else:
 		resp = jsonify(errors)
